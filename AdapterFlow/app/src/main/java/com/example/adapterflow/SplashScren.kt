84eb591b47ef509342d8e1ef0dfeacfd1b6b6710
Aditya Kapadiya <kapadiyaaditya721@gmail.com>
2022-04-24 20:06:15 +0000

    Initial commit

---
+++ AdapterFlow/app/src/main/java/com/example/adapterflow/SplashScren.kt
@@ -0,0 +1,14 @@
+package com.example.adapterflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScren : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_scren)
+
+
+
+    }
+}
